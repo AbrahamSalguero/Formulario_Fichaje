6526e15ca750718792aa15f3e65a0bd5757d9889
Abraham <96110567+AbrahamSalguero@users.noreply.github.com>
2023-08-19 22:02:33 +0200

    Update main.js

--- Timing/js/main.js
+++ Timing/js/main.js
@@ -2,7 +2,9 @@
 
 
 let boton = document.querySelector('#boton_aceptar');
+let text = document.querySelector('.aceptado');
 
-boton.addEventListener('click', () => {   
-      window.alert("Bienvenido al trabajo!");
+boton.addEventListener('click', () => { 
+  // window.alert("hola");  
+    window.open("../html/bienvenida.html");
   });
